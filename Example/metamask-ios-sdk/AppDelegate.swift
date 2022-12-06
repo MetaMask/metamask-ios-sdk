e67c1308ab033cde48ed9da46fa7b6ff2587646a
Mpendulo Ndlovu <mpendulo@elefantel.com>
2022-12-07 00:29:36 +0200

    Add ability to receive socketio events in background

--- Example/metamask-ios-sdk/AppDelegate.swift
+++ Example/metamask-ios-sdk/AppDelegate.swift
@@ -7,12 +7,12 @@
 //
 
 import UIKit
+import metamask_ios_sdk
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -22,6 +22,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+        MetaMaskBackgroundTask.start()
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
@@ -35,12 +36,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        MetaMaskBackgroundTask.stop()
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
 }
-
