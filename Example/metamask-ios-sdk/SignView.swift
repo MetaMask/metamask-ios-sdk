8a5fc54c7b73a4e5a0628b15e4c756cc24ab42ad
Mpendulo Ndlovu <mpendulo@elefantel.com>
2022-12-22 08:40:02 +0200

    Demo app improvements

--- Example/metamask-ios-sdk/SignView.swift
+++ Example/metamask-ios-sdk/SignView.swift
@@ -10,7 +10,7 @@ import metamask_ios_sdk
 struct SignView: View {
     @ObservedObject var ethereum: Ethereum = Ethereum.shared
     
-    @State var textValue = "{\"domain\":{\"chainId\":5,\"name\":\"Ether Mail\",\"verifyingContract\":\"0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC\",\"version\":\"1\"},\"message\":{\"contents\":\"Hello, Bob!\",\"from\":{\"name\":\"Cow\",\"wallets\":[\"0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826\",\"0xDeaDbeefdEAdbeefdEadbEEFdeadbeEFdEaDbeeF\"]},\"to\":[{\"name\":\"Bob\",\"wallets\":[\"0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB\",\"0xB0BdaBea57B0BDABeA57b0bdABEA57b0BDabEa57\",\"0xB0B0b0b0b0b0B000000000000000000000000000\"]}]},\"primaryType\":\"Mail\",\"types\":{\"EIP712Domain\":[{\"name\":\"name\",\"type\":\"string\"},{\"name\":\"version\",\"type\":\"string\"},{\"name\":\"chainId\",\"type\":\"uint256\"},{\"name\":\"verifyingContract\",\"type\":\"address\"}],\"Group\":[{\"name\":\"name\",\"type\":\"string\"},{\"name\":\"members\",\"type\":\"Person[]\"}],\"Mail\":[{\"name\":\"from\",\"type\":\"Person\"},{\"name\":\"to\",\"type\":\"Person[]\"},{\"name\":\"contents\",\"type\":\"string\"}],\"Person\":[{\"name\":\"name\",\"type\":\"string\"},{\"name\":\"wallets\",\"type\":\"address[]\"}]}}"
+    @State var message = "{\"domain\":{\"chainId\":5,\"name\":\"Ether Mail\",\"verifyingContract\":\"0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC\",\"version\":\"1\"},\"message\":{\"contents\":\"Hello, Bob!\",\"from\":{\"name\":\"Cow\",\"wallets\":[\"0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826\",\"0xDeaDbeefdEAdbeefdEadbEEFdeadbeEFdEaDbeeF\"]},\"to\":[{\"name\":\"Bob\",\"wallets\":[\"0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB\",\"0xB0BdaBea57B0BDABeA57b0bdABEA57b0BDabEa57\",\"0xB0B0b0b0b0b0B000000000000000000000000000\"]}]},\"primaryType\":\"Mail\",\"types\":{\"EIP712Domain\":[{\"name\":\"name\",\"type\":\"string\"},{\"name\":\"version\",\"type\":\"string\"},{\"name\":\"chainId\",\"type\":\"uint256\"},{\"name\":\"verifyingContract\",\"type\":\"address\"}],\"Group\":[{\"name\":\"name\",\"type\":\"string\"},{\"name\":\"members\",\"type\":\"Person[]\"}],\"Mail\":[{\"name\":\"from\",\"type\":\"Person\"},{\"name\":\"to\",\"type\":\"Person[]\"},{\"name\":\"contents\",\"type\":\"string\"}],\"Person\":[{\"name\":\"name\",\"type\":\"string\"},{\"name\":\"wallets\",\"type\":\"address[]\"}]}}"
     
     @State private var cancellables: Set<AnyCancellable> = []
     
@@ -22,16 +22,16 @@ struct SignView: View {
         GeometryReader { geometry in
             Form {
                 Section {
-                    Text("Input")
+                    Text("Message")
                         .font(.callout)
-                    TextEditor(text: $textValue)
+                    TextEditor(text: $message)
                         .modifier(TextCaption())
                         .frame(height: geometry.size.height / 2)
                         .modifier(TextCurvature())
                 }
                 
                 Section {
-                    Text("Output")
+                    Text("Result")
                         .font(.callout)
                     TextEditor(text: $result)
                         .modifier(TextCaption())
@@ -43,12 +43,12 @@ struct SignView: View {
                     Button {
                         signInput()
                     } label: {
-                        Text("Sign Text")
+                        Text("Sign")
                             .frame(maxWidth: .infinity, maxHeight: 32)
                     }
                     .alert(isPresented: $showError) {
                         Alert(
-                            title: Text("Signing Error"),
+                            title: Text("Authorization Error"),
                             message: Text(errorMessage)
                         )
                     }
@@ -64,7 +64,7 @@ struct SignView: View {
     
     func signInput() {
         let from = ethereum.selectedAddress
-        let params: [String] = [from, textValue]
+        let params: [String] = [from, message]
         let signRequest = EthereumRequest(
             method: .signTypedDataV4,
             params: params)
