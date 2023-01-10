aa7d42078298f1c4d954e36806951f8e26abc6ee
Mpendulo Ndlovu <mpendulo@elefantel.com>
2023-01-10 22:59:13 +0200

    Add background task handling in sdk main module

--- Example/Pods/Target Support Files/Pods-metamask-ios-sdk_Tests/Pods-metamask-ios-sdk_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_metamask_ios_sdk_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_metamask_ios_sdk_Tests
-@end
--- Example/metamask-ios-sdk/AppDelegate.swift
+++ Example/metamask-ios-sdk/AppDelegate.swift
@@ -18,7 +18,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillResignActive(_: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-        BackgroundTaskManager.start()
     }
 
     func applicationDidEnterBackground(_: UIApplication) {
@@ -32,7 +31,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(_: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-        BackgroundTaskManager.stop()
     }
 
     func applicationWillTerminate(_: UIApplication) {
