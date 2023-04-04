112d30daac5f44bef4549177e70942bb2003a91c
Mpendulo Ndlovu <mpendulo@elefantel.com>
2023-04-04 11:02:19 +0200

    Remove automatic code signing for tests target (#68)

--- Sources/metamask-ios-sdk/Classes/Ethereum/EthereumRequest.swift
+++ Sources/metamask-ios-sdk/Classes/Ethereum/EthereumRequest.swift
@@ -27,7 +27,7 @@ public struct EthereumRequest<T: CodableData>: CodableData {
 
     public func socketRepresentation() -> NetworkData {
         [
-            "id": id ?? "",
+            "id": (id ?? "") as String,
             "method": method,
             "parameters": try? params.socketRepresentation()
         ]
