824d77668f75fcbc1338c7422998b2c364a47a91
Mpendulo Ndlovu <mpendulo@elefantel.com>
2023-09-06 21:01:35 +0200

    Update Swift Package with bumped version of socket io

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/socketio/socket.io-client-swift", .upToNextMajor(from: "16.0.1"))
+        .package(url: "https://github.com/socketio/socket.io-client-swift", .upToNextMajor(from: "16.1.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
