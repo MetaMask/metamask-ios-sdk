4be3d7c9d28dc962a136d67494fabb399685bcf2
Mpendulo Ndlovu <mpendulo@elefantel.com>
2024-02-06 10:48:00 +0200

    Make DeviceInfo struct public

--- Device/DeviceInfo.swift
+++ Device/DeviceInfo.swift
@@ -6,6 +6,6 @@
 import UIKit
 import Foundation
 
-struct DeviceInfo {
-    static let platformDescription = "\(UIDevice.current.name) \(UIDevice.current.systemName) \(UIDevice.current.systemVersion)"
+public struct DeviceInfo {
+    public static let platformDescription = "\(UIDevice.current.name) \(UIDevice.current.systemName) \(UIDevice.current.systemVersion)"
 }
