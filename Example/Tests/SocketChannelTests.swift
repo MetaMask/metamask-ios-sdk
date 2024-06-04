9cdcafd18f11a69519532402bcc9d5697bf809a5
Mpendulo Ndlovu <mpendulo@elefantel.com>
2024-06-04 10:53:01 +0200

    feature: socketchannel unit tests

---
+++ Example/Tests/SocketChannelTests.swift
@@ -0,0 +1,69 @@
+//
+//  SocketChannelTests.swift
+//  metamask-ios-sdk_Tests
+//
+
+import XCTest
+@testable import metamask_ios_sdk
+
+class SocketChannelTests: XCTestCase {
+    var socketChannel: SocketChannel!
+    var mockSocket: MockSocketClient!
+    var mockSocketManager: MockSocketManager!
+    
+    override func setUp() {
+        super.setUp()
+        mockSocketManager = MockSocketManager()
+        socketChannel = SocketChannel(url: "http://mockurl.com")
+        mockSocket = mockSocketManager.standardSocket as? MockSocketClient
+        socketChannel.socket = mockSocket
+        socketChannel.socketManager = mockSocketManager
+    }
+    
+    func testNetworkUrl() {
+        XCTAssertEqual(socketChannel.networkUrl, "http://mockurl.com")
+        
+        socketChannel.networkUrl = "http://newurl.com"
+        XCTAssertEqual(socketChannel.networkUrl, "http://newurl.com")
+    }
+    
+    func testIsConnected() {
+        mockSocket.status = .connected
+        XCTAssertTrue(socketChannel.isConnected)
+        
+        mockSocket.status = .disconnected
+        XCTAssertFalse(socketChannel.isConnected)
+    }
+    
+    func testConnect() {
+        socketChannel.connect()
+        XCTAssertTrue(mockSocket.connectCalled)
+    }
+    
+    func testDisconnect() {
+        socketChannel.disconnect()
+        XCTAssertTrue(mockSocket.disconnectCalled)
+    }
+    
+    func testTearDown() {
+        socketChannel.tearDown()
+    }
+    
+    func testOnClientEvent() {
+        socketChannel.on(.connect) { _ in }
+        
+        XCTAssertTrue(mockSocket.called(.connect))
+    }
+    
+    func testOnStringEvent() {
+        socketChannel.on("testEvent") { _ in }
+
+        XCTAssertTrue(mockSocket.called("testEvent"))
+    }
+    
+    func testEmit() {
+        socketChannel.emit("testEvent", "testData")
+        XCTAssertTrue(mockSocket.emitCalled)
+    }
+}
+
---
+++ Sources/metamask-ios-sdk/Classes/CommunicationLayer/SocketCommLayer/SocketClientProtocol.swift
@@ -0,0 +1,29 @@
+//
+//  SocketProtocol.swift
+//
+
+import SocketIO
+
+protocol SocketProtocol {
+    var status: SocketIOStatus { get }
+    func connect(withPayload payload: [String: Any]?)
+    func disconnect()
+    func emit(_ event: String, _ items: SocketData..., completion: (() -> ())?)
+    @discardableResult
+    func on(clientEvent event: SocketClientEvent, callback: @escaping ([Any], SocketAckEmitter) -> ()) -> UUID
+    @discardableResult
+    func on(_ event: String, callback: @escaping ([Any], SocketAckEmitter) -> Void) -> UUID
+    func removeAllHandlers()
+}
+
+protocol SocketManagerProtocol {
+    var standardSocket: SocketProtocol { get }
+}
+
+extension SocketIOClient: SocketProtocol { }
+
+extension SocketManager: SocketManagerProtocol {
+    var standardSocket: SocketProtocol {
+        self.defaultSocket as SocketProtocol
+    }
+}
