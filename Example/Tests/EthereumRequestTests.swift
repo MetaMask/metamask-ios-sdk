fd138dcae6d623c74a1364f7bf940607ec81a241
Omridan159 <omridan159@gmail.com>
2024-06-13 16:46:10 +0300

    chore: add unit tests to 'EthereumRequestTests'

--- Example/Tests/EthereumRequestTests.swift
+++ Example/Tests/EthereumRequestTests.swift
@@ -7,7 +7,6 @@ import XCTest
 @testable import metamask_ios_sdk
 
 class EthereumRequestTests: XCTestCase {
-    
     func testInitializationWithStringMethod() {
         let params = ["from": "0x1234567890", "to": "0x0987654321"]
         let request = EthereumRequest(id: "12345", method: "eth_sendTransaction", params: params)
@@ -45,7 +44,7 @@ class EthereumRequestTests: XCTestCase {
     
     func testUnknownMethodTypeConversion() {
         let params = ["from": "0x1234567890", "to": "0x0987654321"]
-        let unknownRequest = EthereumRequest(id: "12345", method: "eth_unknown_method", params: params)
+        let unknownRequest = EthereumRequest(id: "12345", method: "eth_unkown_method", params: params)
 
         XCTAssertEqual(unknownRequest.methodType, .unknownMethod)
     }
@@ -70,52 +69,31 @@ class EthereumRequestTests: XCTestCase {
         XCTAssertEqual(socketRep["parameters"] as? [String], params)
     }
     
-    func testEmptyParamsInitialization() {
-        let request = EthereumRequest(id: "12345", method: "eth_sendTransaction")
+    func testStructSocketRepresentation() {
+        let transaction = Transaction(
+            to: "0x0000000000000000000000000000000000000000",
+            from: "0x1234567890",
+            value: "0x000000000000000001"
+        )
 
-        XCTAssertEqual(request.id, "12345")
-        XCTAssertEqual(request.method, "eth_sendTransaction")
-        XCTAssertEqual(request.params, "")
-        XCTAssertEqual(request.methodType, .ethSendTransaction)
-    }
-    
-    func testEmptyParamsInitializationWithMethodEnum() {
-        let request = EthereumRequest(id: "12345", method: .ethGetBalance)
+        let parameters: [Transaction] = [transaction]
 
-        XCTAssertEqual(request.id, "12345")
-        XCTAssertEqual(request.method, "eth_getBalance")
-        XCTAssertEqual(request.params, "")
-        XCTAssertEqual(request.methodType, .ethGetBalance)
-    }
-    
-    class MockNetwork: Networking {
-        func post(_ parameters: [String : Any], endpoint: String) async throws -> Data {
-            if let error = error {
-                throw error
-            }
-
-            return responseData ?? Data()
-        }
+        let transactionRequest = EthereumRequest(
+            id: "24680",
+            method: .ethSendTransaction,
+            params: parameters
+        )
         
-        var responseData: Data?
-        var error: Error?
+        let socketRep = transactionRequest.socketRepresentation() as? [String: Any] ?? [:]
         
-        func post(_ parameters: [String : Any], endpoint: Endpoint) async throws -> Data {
-            if let error = error {
-                throw error
-            }
-
-            return responseData ?? Data()
-        }
+        XCTAssertEqual(socketRep["id"] as? String, "24680")
+        XCTAssertEqual(socketRep["method"] as? String, "eth_sendTransaction")
         
-        func fetch<T>(_ Type: T.Type, endpoint: Endpoint) async throws -> T where T : Decodable {
-            return responseData as! T
-        }
-
-            
-        func addHeaders(_ headers: [String : String]) {
-            // Mock implementation
-        }
+        let socketParams = socketRep["parameters"] as? [Transaction] ?? []
+        XCTAssertEqual(socketParams.first?.from, "0x1234567890")
+        XCTAssertEqual(socketParams.first?.to, "0x0000000000000000000000000000000000000000")
+        XCTAssertEqual(socketParams.first?.value, "0x000000000000000001")
+        XCTAssertNil(socketParams.first?.data)
     }
 }
 
@@ -137,19 +115,7 @@ struct Transaction: CodableData {
             "to": to,
             "from": from,
             "value": value,
-            "data": data ?? ""
+            "data": data
         ]
     }
-}
-
-struct TimestampGenerator {
-    static func timestamp() -> String {
-        return "\(Date().timeIntervalSince1970)"
-    }
-}
-
-enum EthereumMethod: String {
-    case ethSendTransaction = "eth_sendTransaction"
-    case ethGetBalance = "eth_getBalance"
-    case unknownMethod
 }
